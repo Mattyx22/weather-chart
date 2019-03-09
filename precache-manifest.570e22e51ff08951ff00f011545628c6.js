ecc91eceac7759cdf6c35def37279d87b0720452
Mateusz Komar <mateuszkomar@MacBook-Pro.local>
2019-03-09 20:21:06 +0100

    Updates

---
+++ precache-manifest.570e22e51ff08951ff00f011545628c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11d7fef522e0e499a12",
+    "url": "/weather-chart-online/static/js/runtime~main.c11d7fef.js"
+  },
+  {
+    "revision": "0dc70d0e438e8f067b2c",
+    "url": "/weather-chart-online/static/js/main.0dc70d0e.chunk.js"
+  },
+  {
+    "revision": "964590acb88ee371326a",
+    "url": "/weather-chart-online/static/js/2.964590ac.chunk.js"
+  },
+  {
+    "revision": "98aede9ffda82b2c85d34ebee5e6b804",
+    "url": "/weather-chart-online/index.html"
+  }
+];
